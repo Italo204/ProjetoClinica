4630e99f6e3c9d057233b8a8756dcdd3265fd96c
Italo204 <italoosantos48@gmail.com>
2023-11-16 20:00:29 -0300

    Primeiro commit

---
+++ src/main/java/br/edu/imepac/ProjetoClinica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.imepac;
+
+/**
+ *
+ * @author italo-santos-mendes
+ */
+public class ProjetoClinica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
